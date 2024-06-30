0ea8f4248cbde8d6104c447f29f6db3b7d57aa4a
Everon <ender.kraya@gmail.com>
2024-06-30 17:06:58 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,114 @@
+import React from 'react'
+import './App.css'
+import './media.css'
+
+function App() {
+
+
+  return (
+    <>
+    <header className='header'>
+      <h2>Frontend Roadmap 2024</h2>
+    </header>
+    <main>
+      <section className='start'>
+          <div className="left">
+            <span className='section__title'>HTML/CSS</span>
+            <ul>
+              <li>HTML5</li>
+              <li>CSS3</li>
+              <li>Flexbox</li>
+              <li>Grid Layout</li>
+              <li>Responsive Design</li>
+            </ul>
+          </div>
+          <div className="center"></div>
+          <div className="right">
+          
+            </div>
+      </section>
+      <section>
+      <div className="left"></div>
+          <div className="center"></div>
+          <div className="right">
+            <span className='section__title'>JavaScript basics</span>
+            <ul>
+              <li>Data types and data structures</li>
+              <li>Basic Operators and Constructs</li>
+              <li>Functions and scopes</li>
+              <li>Asynchronous programming (Promises, async/await)</li>
+              <li>DOM Basics</li>
+            </ul>
+            </div>
+      </section>
+      <section>
+      <div className="left">
+        <span className="section__title">
+          Advanced JavaScript
+        </span>
+        <ul>
+          <li>EcmaScript 6 and newer(Arrow Functions, Destructuring, Spread/Rest)</li>
+          <li>Modules of import and export</li>
+          <li>Web API</li>
+          <li>Event Loop and understanding asynchrony</li>
+        </ul>
+      </div>
+          <div className="center"></div>
+          <div className="right">
+
+            </div>
+      </section>
+      <section>
+      <div className="left"></div>
+          <div className="center"></div>
+          <div className="right">
+            <span className='section__title'>Version Control</span>
+            <ul>
+              <li>Git</li>
+              <li>Github/ Gitlab/ Bitbucket</li>
+              <li>Basic commands and workflows</li>
+            </ul>
+            </div>
+      </section>
+      <section>
+      <div className="left">
+        <span className="section__title">
+          Frameworks and libraries (React)
+        </span>
+        <ul>
+          <li>Basics (JSX, Components, Props, State)</li>
+          <li>Hooks (useState, useEffect, useContext)</li>
+          <li>Routing</li>
+          <li>State Management</li>
+        </ul>
+        <ul>
+          <b>Other fremeworks</b> (optionally)
+          <li>Vue.jsx</li>
+          <li>Angular</li>
+        </ul>
+      </div>
+          <div className="center"></div>
+          <div className="right">
+
+            </div>
+      </section>
+
+
+      </main>
+      <footer>
+        <div className="footer__start">
+        <span>
+        Aibek Everon
+        </span>
+        <a href="https://www.github.com/gridxprime">Github</a>
+        <a href="https://www.t.me/till_morning_comes">Telegram</a>
+        </div>
+        <div className="footer__bottom">
+          <span>2024</span>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default App
